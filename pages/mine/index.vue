ea5f83cd0106cdc93b822efe3dba737007c4ef57
binh <binh@binhs-MacBook-Pro.local>
2018-04-10 10:04:11 +0800

    api-to-xapi

--- pages/mine/index.vue
+++ pages/mine/index.vue
@@ -42,7 +42,7 @@ export default {
     axios.defaults.baseURL = 'http://gongsongping.com'
     // axios.defaults.baseURL = 'http://localhost:3001'
     // let url = context.env.baseUrl+'/posts'
-    let res  = await axios.get('/api/posts?skip=0')
+    let res  = await axios.get('/xapi/posts?skip=0')
     console.log(res.data);
     return { posts: res.data }
     // return { name: context.params }
@@ -54,7 +54,7 @@ export default {
     async sendPost(){
       //  console.log(this);    
        let res = axios({
-         url: '/api/posts',
+         url: '/xapi/posts',
          method:'POST',
          data:{
            content: this.content
@@ -65,7 +65,7 @@ export default {
     },
     async infiniteHandler($state) {
        this.skip = this.skip+1
-       let res  = await axios.get('/api/posts?skip='+this.skip)
+       let res  = await axios.get('/xapi/posts?skip='+this.skip)
        console.log(res.data);
        this.posts = this.posts.concat(res.data)
        $state.loaded()
