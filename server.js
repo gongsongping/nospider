e2228d51bec3eef679456fe4d385f6d12a2d8bff
gongsongping <gongsongping@gmai.com>
2018-04-28 10:45:22 +0800

    use-server.js

--- server.js
+++ server.js
@@ -10,11 +10,6 @@ const nuxt = new Nuxt(config)
 // // if (config.dev) {
 // // }
 
-new Builder(nuxt).build()
-.catch((error) => {
-    console.error(error)
-    process.exit(1)
-})
 
 app.get('/test', function  (req, res, next) {
     res.json({test:'test'})
@@ -22,10 +17,20 @@ app.get('/test', function  (req, res, next) {
 
 app.use(nuxt.render)
 
+new Builder(nuxt).build()
+.then(function () {
+    console.log('-----build over------');
+      app.listen(3000,'0.0.0.0',function () {
+        console.log('--nuxt------listen',3000,'------',config.dev);
+    });
+})
+.catch((error) => {
+    console.error(error)
+    process.exit(1)
+})
 
-app.listen(3000,'0.0.0.0',function () {
-    console.log('--nuxt------listen',3000,'------');
-    // nuxt.showOpen()
-});
+// setTimeout(()=>{
+  
+// },8000)
 
 // nuxt.listen(3000, '0.0.0.0')
