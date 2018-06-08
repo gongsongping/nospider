363b7fd63fae6532716fa1cc297b6b1ba82a5a27
gongsongping <gongsongping@gmail.com>
2018-06-09 06:07:36 +0800

    server-change

--- server.js
+++ server.js
@@ -21,43 +21,43 @@ app.use(bodyParser.json());
 app.use(cors());
 
 
-const jsdom = require('jsdom');
-const {
-  JSDOM
-} = jsdom;
+// const jsdom = require('jsdom');
+// const {
+//   JSDOM
+// } = jsdom;
 const jsonfile = require('jsonfile')
-const phantom = require('phantom');
+// const phantom = require('phantom');
 
 
-const getjq = (JSDOM, url, html) => {
-  let {
-    window
-  } = new JSDOM(html, {
-    url: url.origin,
-    referrer: url.origin,
-    contentType: "text/html",
-    userAgent: "Mellblomenator/9000",
-    includeNodeLocations: true
-  });
-  let $ = require('jquery')(window);
-  return $
-}
+// const getjq = (JSDOM, url, html) => {
+//   let {
+//     window
+//   } = new JSDOM(html, {
+//     url: url.origin,
+//     referrer: url.origin,
+//     contentType: "text/html",
+//     userAgent: "Mellblomenator/9000",
+//     includeNodeLocations: true
+//   });
+//   let $ = require('jquery')(window);
+//   return $
+// }
 
-const getobj = (fds, $) => {
-  let obj = {}
-  fds.forEach((f, idx) => {
-    if (f.type === 'text') {
-      obj[f.name] = $(f.path).text()
-    }
-    if (f.type === 'image') {
-      obj[f.name] = $(f.path).attr('src')
-    }
-    if (f.type === 'link') {
-      obj[f.name] = $(f.path).attr('href')
-    }
-  })
-  return obj
-}
+// const getobj = (fds, $) => {
+//   let obj = {}
+//   fds.forEach((f, idx) => {
+//     if (f.type === 'text') {
+//       obj[f.name] = $(f.path).text()
+//     }
+//     if (f.type === 'image') {
+//       obj[f.name] = $(f.path).attr('src')
+//     }
+//     if (f.type === 'link') {
+//       obj[f.name] = $(f.path).attr('href')
+//     }
+//   })
+//   return obj
+// }
 
 app.get('/test', function (req, res, next) {
   console.log(req.query.q);
@@ -92,109 +92,107 @@ app.post('/napi/url', async function (req, res, next) {
 })
 
 
-app.post('/napi/scrawl', async function (req, res, next) {
-  console.log('----------body-parser-body', req.body);
-  const rule = req.body.rule
+// app.post('/napi/scrawl', async function (req, res, next) {
+//   console.log('----------body-parser-body', req.body);
+//   const rule = req.body.rule
 
-  const url = new URL(rule.detailurl);
-  const detailword = url.pathname.split('/')[1]
-  const urlps = rule.urlps
-  console.log('-----detailword---', detailword, '---urlps---', urlps, '---fields--', rule.fields);
-
-
-  (async function () {
-    const instance = await phantom.create();
-    const page = await instance.createPage();
-    await page.on('onResourceRequested', function (requestData) {
-      console.info('Requesting', requestData.url);
-    });
-
-    await urlps.forEach(async function (p, idx) {
-      console.log('-----scrawling-fullurl--', rule.first + p + rule.third);
-
-      const status = await page.open(rule.first + p + rule.third); //https://stackoverflow.com/ ;//https://www.google.com
-      await page.includeJs("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js")
-
-      const alinks = await page.evaluate(function (detailword) {
-        //   console.log('----------title-inside', document.title);
-        //   return document.title
-        return $('a').filter('[href*=' + detailword + ']')
-      })
-      await alinks.each(async function (i, a) {
-        console.log('----------scrawling-page--', a.href);
-        const status = await page.open(a.href);
-        await page.includeJs("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js")
-        const ob = await page.evaluate(function () {
-          let obj = getobj(rule.fields, $)
-          obj['url'] = a.href
-
-          console.log('----obj----', obj);
-          return obj
-        })
-        await jsonfile.writeFile('./tmp/test.json', ob, {
-          flag: 'a'
-        }, function (err) {
-          console.error(err)
-        })
-      })
-    })
-    setTimeout(async () => {
-      await instance.exit();
-
-    }, 120000)
-
-  })();
+//   const url = new URL(rule.detailurl);
+//   const detailword = url.pathname.split('/')[1]
+//   const urlps = rule.urlps
+//   console.log('-----detailword---', detailword, '---urlps---', urlps, '---fields--', rule.fields);
 
 
-  // urlps.forEach((p, idx) => {
-  //     console.log('-----fullurl--',rule.first+p+rule.third);
-  //     axios.get(rule.first+p+rule.third)
-  //     .then(function (resp) {
-  //         console.log('-----scrawling-page--',p,'---');
-  //         // let $ = cheerio.load(resp.data);
-  //         // fs.appendFileSync('hackernews.txt', title + '\n' + link + '\n');
+//   (async function () {
+//     const instance = await phantom.create();
+//     const page = await instance.createPage();
+//     await page.on('onResourceRequested', function (requestData) {
+//       console.info('Requesting', requestData.url);
+//     });
 
-  //         let $ = getjq(JSDOM,url,resp.data)
-  //         // let $ = cheerio.load(resp.data)             
+//     await urlps.forEach(async function (p, idx) {
+//       console.log('-----scrawling-fullurl--', rule.first + p + rule.third);
 
-  //         $('a').filter(`[href*=${detailword}]`).each(function(i,a){
-  //             console.log('------',a.href)
-  //             axios.get(a.href)
-  //             .then(function (respo) {
-  //                 // console.log('-----detaildom',respo.data);
-  //                 // let $ = getjq(JSDOM,url,respo.data)
-  //                 // let obj = getobj(rule.fields,$)
-  //                 // obj['url'] = a.href
+//       const status = await page.open(rule.first + p + rule.third); //https://stackoverflow.com/ ;//https://www.google.com
+//       await page.includeJs("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js")
 
-  //                 const $ = cheerio.load(respo.data) 
-  //                 let obj = getobj(rule.fields,$)
-  //                 obj['url'] = a.href
+//       const alinks = await page.evaluate(function (detailword) {
+//         //   console.log('----------title-inside', document.title);
+//         //   return document.title
+//         return $('a').filter('[href*=' + detailword + ']')
+//       })
+//       await alinks.each(async function (i, a) {
+//         console.log('----------scrawling-page--', a.href);
+//         const status = await page.open(a.href);
+//         await page.includeJs("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js")
+//         const ob = await page.evaluate(function () {
+//           let obj = getobj(rule.fields, $)
+//           obj['url'] = a.href
 
-  //                 // let obj = { url: a.href, html: respo.data}
-  //                 // let obj = { url: a.href, comment: $('.comment').text()}
-  //                 console.log('----obj----', obj);
-  //                 jsonfile.writeFile('./tmp/test.json', obj, {flag: 'a'}, function (err) {
-  //                     console.error(err)
-  //                 })
-  //             })
-  //         })
+//           console.log('----obj----', obj);
+//           return obj
+//         })
+//         await jsonfile.writeFile('./tmp/test.json', ob, {
+//           flag: 'a'
+//         }, function (err) {
+//           console.error(err)
+//         })
+//       })
+//     })
+//     setTimeout(async () => {
+//       await instance.exit();
 
-  //     })
-  // });
+//     }, 120000)
+
+//   })();
 
 
-  res.json({
-    ok: 'ok'
-  })
+//   // urlps.forEach((p, idx) => {
+//   //     console.log('-----fullurl--',rule.first+p+rule.third);
+//   //     axios.get(rule.first+p+rule.third)
+//   //     .then(function (resp) {
+//   //         console.log('-----scrawling-page--',p,'---');
+//   //         // let $ = cheerio.load(resp.data);
+//   //         // fs.appendFileSync('hackernews.txt', title + '\n' + link + '\n');
 
-})
+//   //         let $ = getjq(JSDOM,url,resp.data)
+//   //         // let $ = cheerio.load(resp.data)             
+
+//   //         $('a').filter(`[href*=${detailword}]`).each(function(i,a){
+//   //             console.log('------',a.href)
+//   //             axios.get(a.href)
+//   //             .then(function (respo) {
+//   //                 // console.log('-----detaildom',respo.data);
+//   //                 // let $ = getjq(JSDOM,url,respo.data)
+//   //                 // let obj = getobj(rule.fields,$)
+//   //                 // obj['url'] = a.href
+
+//   //                 const $ = cheerio.load(respo.data) 
+//   //                 let obj = getobj(rule.fields,$)
+//   //                 obj['url'] = a.href
+
+//   //                 // let obj = { url: a.href, html: respo.data}
+//   //                 // let obj = { url: a.href, comment: $('.comment').text()}
+//   //                 console.log('----obj----', obj);
+//   //                 jsonfile.writeFile('./tmp/test.json', obj, {flag: 'a'}, function (err) {
+//   //                     console.error(err)
+//   //                 })
+//   //             })
+//   //         })
+
+//   //     })
+//   // });
+
+
+//   res.json({
+//     ok: 'ok'
+//   })
+
+// })
 
 
 
 app.use(nuxt.render)
-// Build only in dev mode
-// // if (config.dev) {
-// // }
+
 new Builder(nuxt).build()
   .then(function () {
     console.log('-----build over------');
